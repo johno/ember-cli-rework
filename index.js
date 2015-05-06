4f63bead66cbc3ed778f91ccd95edbfd3c3115ff
ismay <ismay@ismaywolff.nl>
2015-05-06 13:36:46 +0200

    Refactor according to cssnext example
    - reworkTree function doesn't seem to work yet

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-'use strict';
+// 'use strict';
 
 var rework = require('broccoli-rework');
+var Funnel = require('broccoli-funnel');
 
-module.exports = {
-  name: 'ember-cli-rework',
+/**
+ * Preprocessor
+ */
 
-  included: function(app) {
-    this.app = app;
-    this.plugins = this.app.options.reworkPlugins;
-  },
+// Preprocessor constructor
+function Preprocessor(options) {
+  this.name = 'ember-cli-rework';
+  this.options = options || {};
+}
 
-  postprocessTree: function(type, tree) {
-    var plugins = this.plugins;
-    if (plugins && (type === 'all' || type === 'styles')) {
+// Preprocessor.toTree method
+Preprocessor.prototype.toTree = function(tree, inputPath, outputPath, inputOptions) {
+  // Apply rework plugins to css
+  var reworkTree = function reworkTree(tree) {
+    var plugins = this.options.plugins;
+
+    if (plugins) {
       tree = rework(tree, { use: function(css) {
         plugins.forEach(function(plugin) {
           css.use(plugin);
@@ -20,6 +27,28 @@ module.exports = {
       }});
     }
 
+    // Return the modified css
     return tree;
+  };
+
+  // Passes rework's ouput to broccoli-funnel
+  return new Funnel(reworkTree(tree), {
+    // Broccoli-funnel options
+    srcDir: inputPath,
+    destDir: outputPath
+  });
+};
+
+/**
+ * Export
+ */
+
+// Export rework preprocessor
+module.exports = {
+  name: 'ember-cli-rework',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    app.registry.add('css', new Preprocessor(app.options && app.options.rework));
   }
 };
